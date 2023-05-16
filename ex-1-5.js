9cdad85b9ea8ce25771d1cb5a11bbdcdd77b8685
David Andersson <lejonmanen@gmail.com>
2023-05-16 10:33:37 +0200

    Exercise 1.5 started

---
+++ ex-1-5.js
@@ -0,0 +1,20 @@
+// Övning 1.5
+// Bara boll
+
+// Medlem - en entitet, ett JS - objekt
+
+const medlem = {
+	namn,
+	lag,  // vilket lag är man med i
+	medlemTill,  // när går medlemskapet ut
+	telefon,
+	epost,
+	statistik: {
+		antalMål,
+		antalSpeladeMatcher,
+		antalVunna,
+		antalFörlorade,
+		lyckadePassningar  // procent
+		// osv...
+	}
+}
